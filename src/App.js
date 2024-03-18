23b2561c0fe0521e44145f69636fadafaf88b1f8
LayMui <mui_ume@hotmail.com>
2024-03-18 15:11:33 +0800

    Add Form

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ export default function App() {
   return <div className="app">
     <div className="sidebar">
     <FriendsList/>
+    <FormAddFriend/>
     </div>
   </div>
 }
@@ -32,14 +33,38 @@ function FriendsList() {
   
   return (
     <ul>
-      {friends.map((friend) => {
+      {friends.map((friend) => (
         <Friend friend={friend} key={friend.id}/>
-    })}
+    ))}
     </ul>
   );
 }
 
 function Friend({friend})
 {
-  return <li>{friend.name}</li>;
+  return <li>
+    <img src={friend.image} alt={friend.name} />
+    <h3>{friend.name}</h3>
+    {friend.balance < 0 && <p className="red">You owe {friend.name} {Math.abs(friend.balance)}</p>}
+    {friend.balance > 0 && <p className="green">{friend.name} owes you {Math.abs(friend.balance)}</p>}
+    {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+    <Button>Select</Button>
+    </li>;
+}
+
+function Button({children})
+{
+  return <button className="button">{children}</button>
+}
+
+function FormAddFriend() {
+
+  return <form className="form-add-friend">
+    <label>🎆 Friend name</label>
+    <input type="text"/>
+    <label>🎇 Image URL</label>
+    <input type="text"/>
+    <Button>Add</Button>
+  </form>
+
 }
